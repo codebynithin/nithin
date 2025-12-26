3f677d5ca431fd0136763cf2cec07596f6db4fca
Nithin V <nithinviswanathan@wimoku.com>
2025-12-26 09:38:01 +0530

    feat: banners added

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.scss';
 import Content from './components/content/Content';
 import { DialogProvider } from './components/dialog/Dialog';
+import Banner from './components/banner/Banner';
 
 const App: React.FC = () => {
   return (
     <DialogProvider>
+      <Banner />
       <div className="w-full text-sm md:text-lg flex flex-col justify-between m-0">
         <Content />
       </div>
